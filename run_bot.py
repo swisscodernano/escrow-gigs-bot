482cdff8ed9561591d1eac95e85956bfbe6bf335
swisscodernano <brundo@gmail.com>
2025-08-13 23:12:00 +0200

    fix(runtime): Aggiunge dinamicamente il sys.path in run_bot.py

--- run_bot.py
+++ run_bot.py
@@ -1,4 +1,10 @@
+import sys
+import os
 import asyncio
+
+# Add the project root to the Python path
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+
 from app.telegram_bot import main
 
 if __name__ == "__main__":
